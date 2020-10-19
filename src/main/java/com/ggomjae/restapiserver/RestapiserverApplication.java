9399c99b8e3ca8c5c99300aa5c6f283363b0686d
ggomjae <ggomjae@naver.com>
2020-10-19 14:06:47 +0900

    Initial commit

---
+++ src/main/java/com/ggomjae/restapiserver/RestapiserverApplication.java
@@ -0,0 +1,13 @@
+package com.ggomjae.restapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestapiserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ggomjae/restapiserver/RestapiserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggomjae.restapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
